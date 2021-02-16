0835b8107c2c7bb868ee19854c78c7128996b2e0
fschn <fschnabel007@gmail.com>
2021-02-17 00:16:18 +0100

    Database with autoincrementing Key

--- src/main/SQL.java
+++ src/main/SQL.java
@@ -5,123 +5,132 @@ import main.Model.Field;
 import java.sql.*;
 
 public class SQL {
-	public int sid = 0; //Sudoku ID
-	private int hid = 0; //Highscore ID
 	private static final String URL = "jdbc:sqlite:db/Sudoku.db";
 	
 	public SQL() {
-		
-		try (Connection conn = DriverManager.getConnection(URL)) {
-			Statement stmt = conn.createStatement();
-			sid = stmt.executeQuery("SELECT * FROM sudoku ORDER BY id DESC LIMIT 1").getInt(1) + 1;
-			stmt.close();
-			
-			Statement stmt2 = conn.createStatement();
-			hid = stmt2.executeQuery("SELECT * FROM high ORDER BY id DESC LIMIT 1").getInt(1) + 1;
-			System.out.println(hid);
-			stmt2.close();
-		} catch (SQLException ignored) {
-		}
-		newtable();
-	}
-	
-	
-	public void newtable() {
 		try (Connection conn = DriverManager.getConnection(URL);
-			 Statement stmt = conn.createStatement();
-			 Statement stmt2 = conn.createStatement()) {
+			 Statement stmt = conn.createStatement()) {
 			
 			// create a new table
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS sudoku (id int PRIMARY KEY AUTOINCREMENT, werte varchar(163), time int)");
-			stmt2.executeUpdate("CREATE TABLE IF NOT EXISTS high (id int PRIMARY KEY AUTOINCREMENT, name String, time int)");
-			
-		} catch (SQLException ignored) {
+			stmt.executeUpdate("""
+					CREATE TABLE IF NOT EXISTS sudoku (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						werte varchar(163),
+						time int
+					);
+					CREATE TABLE IF NOT EXISTS high (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						name String,
+						time int
+					);""");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void insert(Field[][] feld, int time) {
-		StringBuilder s = new StringBuilder();
-		for (int y = 0; y < 9; y++) {
-			for (int x = 0; x < 9; x++) {
-				s.append(feld[x][y].getInhalt());
-				s.append(feld[x][y].isConstant() ? "t" : "f");
-			}
-		}
+	public static void main(String[] args) {
 		try (Connection conn = DriverManager.getConnection(URL);
 			 Statement stmt = conn.createStatement()) {
 			
-			stmt.executeUpdate("INSERT INTO sudoku VALUES(" + sid + ",'" + s + "', " + time + ")");
-			sid++;
+			
+			stmt.executeUpdate("DROP TABLE sudoku; DROP TABLE high");
+//			System.out.println(x);
+//			stmt.executeUpdate("INSERT INTO sudoku2ada(werte,time) VALUES('test', 25)");
 		} catch (SQLException e) {
-			System.out.println("test3");
-			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void save(String field, long time) {
+
+		try (Connection conn = DriverManager.getConnection(URL);
+			 PreparedStatement ps = conn.prepareStatement("INSERT INTO sudoku(werte, time) VALUES(?, ?)")) {
+			ps.setString(1, field);
+			ps.setLong(2, time);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
 	}
 	
-	public void insert(String name, int time) {
+	public void insertHighscore(String name, long time) {
 		try (Connection conn = DriverManager.getConnection(URL);
-			 Statement stmt = conn.createStatement()) {
-			stmt.executeUpdate("INSERT INTO high VALUES(" + hid + ",'" + name + "', " + time + ")");
-			
-			hid++;
+			 PreparedStatement ps = conn.prepareStatement("INSERT INTO high(name, time) VALUES(?, ?)")) {
+			ps.setString(1, name);
+			ps.setLong(2, time);
+			ps.executeUpdate();
 		} catch (SQLException e) {
-			System.out.println("test4");
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 	
-	public String sget() {
-		String s = "";
-		int idtmp = sid - 1;
+	public boolean isSudokuEmpty(){
 		try (Connection conn = DriverManager.getConnection(URL);
 			 Statement stmt = conn.createStatement();
-			 Statement stmt2 = conn.createStatement()) {
-			
-			ResultSet rs = stmt.executeQuery("SELECT * FROM sudoku WHERE id = " + idtmp);
-			s = rs.getString("werte") + rs.getInt("time");
-			rs.close();
-			
-			stmt2.executeUpdate("DELETE FROM sudoku WHERE id = " + idtmp);
-			sid--;
+			 ResultSet rs = stmt.executeQuery("SELECT * FROM sudoku")) {
+			return rs.isClosed();
 		} catch (SQLException e) {
-			System.out.println("test5");
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
-		
-		
-		return s;
+		return true;
 	}
 	
-	public String[] hget() {
+	public String getSudoku() {
+		try (Connection conn = DriverManager.getConnection(URL);
+			 Statement stmt = conn.createStatement();
+			 ResultSet rs = stmt.executeQuery("SELECT werte FROM sudoku LIMIT 1")) {
+			return rs.getString("werte");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public int getSudokuTime() {
+		try (Connection conn = DriverManager.getConnection(URL);
+			 Statement stmt = conn.createStatement();
+			 ResultSet rs = stmt.executeQuery("SELECT time FROM sudoku LIMIT 1")) {
+			return rs.getInt("time");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public void clearSudoku() {
+		try (Connection conn = DriverManager.getConnection(URL);
+			 Statement stmt = conn.createStatement()) {
+			stmt.executeUpdate("DELETE FROM sudoku");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public String[] getHighscores() {
 		String[] s = new String[11];
-		int i = 0;
+		
 		try (Connection conn = DriverManager.getConnection(URL);
-			 Statement stmt = conn.createStatement()) {
-			
-			ResultSet rs = stmt.executeQuery("SELECT name,time FROM high ORDER BY time LIMIT 10");
-			while (rs.next()) {
-				s[i] = rs.getString("name");
-				s[i] = s[i] + " " + rs.getInt("time");
-				i++;
+			 Statement stmt = conn.createStatement();
+			 ResultSet rs = stmt.executeQuery("SELECT name, time FROM high ORDER BY time LIMIT 10")) {
+			for (int i = 0; rs.next(); i++) {
+				s[i] = rs.getString("name") + " Time:" + rs.getInt("time");
 			}
-			rs.close();
 		} catch (SQLException e) {
-			System.out.println("test6");
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 		return s;
 	}
 	
-	public void delete(int iddel) {
+	public void delete(int id) {
 		try (Connection conn = DriverManager.getConnection(URL);
 			 Statement stmt = conn.createStatement()) {
 			
-			stmt.executeUpdate("DELETE FROM sudoku WHERE id = " + iddel);
+			stmt.executeUpdate("DELETE FROM sudoku WHERE id = " + id);
 			
 		} catch (SQLException e) {
-			System.out.println("test7");
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
